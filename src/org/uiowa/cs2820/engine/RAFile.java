7195e131ce7851030e3d9c3c8e4235b2d0407413
austinforlife <austin-washburn@uiowa.edu>
2014-11-04 13:52:22 -0600

    Progress on free method
    Not really sure how to index just the first two parts, or whether I
    need to index that or not to overwrite the first two bytes. Not sure
    about syntax/ rules for accessing and overwriting the first two bytes.

--- src/org/uiowa/cs2820/engine/RAFile.java
+++ src/org/uiowa/cs2820/engine/RAFile.java
@@ -25,7 +25,6 @@ public class RAFile extends ChunkedAccess
 			file.read(result);
 			file.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -40,17 +39,33 @@ public class RAFile extends ChunkedAccess
 			file.write(objectByteRepr);
 		    file.close();	
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 	}
 
 	@Override
+	// Only need to write first two bytes at chunkPosition to 0. Because of the way
+	// ByteConverter utility works, if it reads [0,0] for first two bytes, that content will
+	// be ignored, and overwritten if needed.
 	public void free(int chunkPosition)
 	{
+		RandomAccessFile file;
+		try {
+			file = new RandomAccessFile(FILE, "rw");
+			file.seek(chunkPosition*chunkSize); // seek chunkPosition
+			byte[] erase = convert(0);
+			file.write(erase); 					// only write first two bytes?
+			file.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			
+	}
+
+	private byte[] convert(int i) {
 		// TODO Auto-generated method stub
-		
+		return null;
 	}
 
 	@Override
