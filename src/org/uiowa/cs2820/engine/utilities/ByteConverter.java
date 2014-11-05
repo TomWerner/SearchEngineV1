f612b73d8fd1e530206e31c0cc59dc5238df1eda
TomWerner <tomwer3@gmail.com>
2014-11-05 15:27:52 -0600

    Added comments to ByteConverter

--- src/org/uiowa/cs2820/engine/utilities/ByteConverter.java
+++ src/org/uiowa/cs2820/engine/utilities/ByteConverter.java
@@ -8,9 +8,23 @@ import org.uiowa.cs2820.engine.ValueFileNode;
 
 public class ByteConverter
 {
+    
+    /*
+     * These byte arrays identify the contents of a chunk.
+     * To check if a chunk is occupied we check to see
+     * the value of the first two bytes of the chunk.
+     * 
+     * If the first two bytes are [0, 1], [1, 1], or [1, 0]
+     * then we know the chunk is occupied. If the first two
+     * bytes are [0, 0] then we treat the entire chunk as empty.
+     * 
+     * This helps with quickly determining if a chunk is empty or not.
+     */
     public static final byte[] BINARY_FILE_NODE = { 0, 1 };
     public static final byte[] VALUE_FILE_NODE = { 1, 1 };
     public static final byte[] FIELD = { 1, 0 };
+    
+    
     public static final int EXISTS_POSITION = 0;
     public static final int EXISTS_SIZE = 2;
     
@@ -26,7 +40,7 @@ public class ByteConverter
             return ValueFileNode.revert(byteRepr);
         else if (Arrays.equals(objType, FIELD))
             return Field.revert(byteRepr);
-        else
+        else //The first two bytes are [0, 0]
             return null;
     }
 }
