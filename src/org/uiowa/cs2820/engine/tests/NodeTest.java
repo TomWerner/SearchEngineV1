76c0c9cb2d707cea567c483a049d64a1480acaae
TomWerner <tomwer3@gmail.com>
2014-11-05 16:48:50 -0600

    Updated gitignore so we don't add .dat files

--- src/org/uiowa/cs2820/engine/tests/NodeTest.java
+++ src/org/uiowa/cs2820/engine/tests/NodeTest.java
@@ -11,14 +11,14 @@ public class NodeTest {
 	public void testNodedelete() {
 		Node node1 = new Node(null, "string");
 		
-		node1.remove("string");
+		node1.delete("string");
 	}
 
     @Test
     public void testNodeadd(){
         Node node2 = new Node(null, "string2");
 
-        node2.remove("string2");
+        node2.delete("string2");
         node2.add("string2");
 
      }
