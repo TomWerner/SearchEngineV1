30b395a9724502154d147755a84acc92fb14e722
TomWerner <tomwer3@gmail.com>
2014-11-06 14:01:39 -0600

    Updated AllTests to run all tests

--- src/org/uiowa/cs2820/engine/tests/AllTests.java
+++ src/org/uiowa/cs2820/engine/tests/AllTests.java
@@ -6,7 +6,8 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ AVLFieldDatabaseTest.class, BinaryFileNodeTest.class, BinaryTreeFieldDatabaseTest.class, FieldTest.class, IdentifierDatabaseTest.class,
-        IntegratedDatabaseWithBinaryTreeTest.class, IntegratedDatabaseWithAVLTest.class, IntegrationTests.class, RAFileTest.class, ValueFileNodeTest.class })
+        IntegratedDatabaseWithAVLTest.class, IntegratedDatabaseWithBinaryTreeTest.class, IntegrationTests.class, IntergratedAVLWithRAFile.class,
+        NodeTest.class, RAFileTest.class, ValueFileNodeTest.class })
 public class AllTests
 {
 
