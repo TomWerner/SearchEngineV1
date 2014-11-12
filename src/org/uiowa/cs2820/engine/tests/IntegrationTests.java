c08cf694108d7c9d74d8430abf3eb8e61547e223
zmilder <zackery-milder@uiowa.edu>
2014-11-12 15:03:04 -0600

    Testing Integration
    Added Indexer as well as Field search however not getting results that i
    thought i would originally. Test may need to be looked over.

--- src/org/uiowa/cs2820/engine/tests/IntegrationTests.java
+++ src/org/uiowa/cs2820/engine/tests/IntegrationTests.java
@@ -2,6 +2,9 @@ package org.uiowa.cs2820.engine.tests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import org.junit.Test;
 import org.uiowa.cs2820.engine.*;
 
@@ -37,5 +40,82 @@ public class IntegrationTests
         assertEquals(2, S.length);
         assertEquals(S[0], "abc");
         assertEquals(S[1], "def");
+    }
+    
+    @Test
+    public void testEmptyDatabaseRAFile(){
+    	
+		File testingFile = new File("testingFile.dat");
+		if(testingFile.exists())
+			testingFile.delete();
+		
+		File testing = new File("testing.dat");
+		if(testing.exists())
+			testing.delete();
+		
+		
+        ChunkedAccess file1 = new RAFile(new File("testingFile.dat"),16, BinaryFileNode.MAX_SIZE);
+        FieldDatabase fieldDB = new BinaryTreeFieldDatabase(file1);
+        ChunkedAccess file2 = new RAFile(new File("testing.dat"), 16, BinaryFileNode.MAX_SIZE);
+        IdentifierDatabase identDB = new IdentifierDatabase(file2);
+        
+        IntegratedFileDatabase database = new IntegratedFileDatabase(fieldDB, identDB);
+        Field field = new Field("name", "value");
+        String identifier = "filename1";
+        Indexer indexer = new Indexer(database, identifier);
+
+        indexer.addField(field);
+
+        ArrayList<String> results = database.fetch(field);
+        assertEquals(1, results.size());
+        assertEquals(identifier, results.get(0));
+    }
+    
+    @Test
+    public void BinaryIntegrationTesting(){
+		File testingFile = new File("testingFile.dat");
+		if(testingFile.exists())
+			testingFile.delete();
+		
+		File testing = new File("testing.dat");
+		if(testing.exists())
+			testing.delete();
+		
+		
+        ChunkedAccess file1 = new RAFile(new File("testingFile.dat"),16, BinaryFileNode.MAX_SIZE);
+        FieldDatabase fieldDB = new BinaryTreeFieldDatabase(file1);
+        ChunkedAccess file2 = new RAFile(new File("testing.dat"), 16, BinaryFileNode.MAX_SIZE);
+        IdentifierDatabase identDB = new IdentifierDatabase(file2);
+        
+        IntegratedFileDatabase database = new IntegratedFileDatabase(fieldDB, identDB);
+        String identifier = "filename1";
+        Indexer indexer = new Indexer(database, identifier);
+        FieldSearch search = new FieldSearch(database);
+
+        
+        Field field1 = new Field("name", "d");
+        Field field2 = new Field("name", "a");
+        Field field3 = new Field("name", "e");
+        Field field4 = new Field("name", "b");
+        Field field5 = new Field("name", "f");
+        Field field6 = new Field("name", "c");
+        Field field7 = new Field("name", "g");
+        
+        indexer.addField(field1);
+        indexer.addField(field2);
+        indexer.addField(field3);
+        indexer.addField(field4);
+        indexer.addField(field5);
+        indexer.addField(field6);
+        indexer.addField(field7);
+        
+        
+      
+        assertEquals(search.findEquals(field3),"e");
+        assertEquals(search.findEquals(field4),"b");
+        assertEquals(search.findEquals(field5),"f");
+        assertEquals(search.findEquals(field6),"c");
+        assertEquals(search.findEquals(field7),"g");
+        
     }
 }
