2cd557379332e042c46778ad0a6f248c6bee02bb
zmilder <zackery-milder@uiowa.edu>
2014-11-14 13:23:33 -0600

    Added Integration tests
    Added two Integration Tests using Indexer, Field Search as well Field.
    Created AVL Integration Test as well as a test for removal for both
    trees

--- src/org/uiowa/cs2820/engine/tests/IntegrationTests.java
+++ src/org/uiowa/cs2820/engine/tests/IntegrationTests.java
@@ -139,4 +139,150 @@ public class IntegrationTests
         assertEquals(identifier, results[0]);
         
     }
+    
+    @Test
+    public void AVLIntegrationTesting(){
+		File testingFile = new File("testingFile.dat");
+		if(testingFile.exists())
+			testingFile.delete();
+		
+		File testing = new File("testing.dat");
+		if(testing.exists())
+			testing.delete();
+		
+		
+        ChunkedAccess file1 = new RAFile(new File("testingFile.dat"),16, BinaryFileNode.MAX_SIZE);
+        FieldDatabase fieldDB = new AVLFieldDatabase(file1);
+        ChunkedAccess file2 = new RAFile(new File("testing.dat"), 16, BinaryFileNode.MAX_SIZE);
+        IdentifierDatabase identDB = new IdentifierDatabase(file2);
+        
+        IntegratedFileDatabase database = new IntegratedFileDatabase(fieldDB, identDB);
+        String identifier = "filename1";
+        Indexer indexer = new Indexer(database, identifier);
+        FieldSearch search = new FieldSearch(database);
+
+        
+        Field field1 = new Field("name", "d");
+        Field field2 = new Field("name", "a");
+        Field field3 = new Field("name", "e");
+        Field field4 = new Field("name", "b");
+        Field field5 = new Field("name", "f");
+        Field field6 = new Field("name", "c");
+        Field field7 = new Field("name", "g");
+        
+        indexer.addField(field1);
+        indexer.addField(field2);
+        indexer.addField(field3);
+        indexer.addField(field4);
+        indexer.addField(field5);
+        indexer.addField(field6);
+        indexer.addField(field7);
+        
+        String[] results = search.findEquals(field1);
+        
+        assertEquals(1, results.length);
+        assertEquals(identifier, results[0]);
+      
+        results = search.findEquals(field2);
+        assertEquals(1, results.length);
+        assertEquals(identifier, results[0]);
+        
+        results = search.findEquals(field3);
+        assertEquals(1, results.length);
+        assertEquals(identifier, results[0]);
+        
+        results = search.findEquals(field4);
+        assertEquals(1, results.length);
+        assertEquals(identifier, results[0]);
+        
+        results = search.findEquals(field5);
+        assertEquals(1, results.length);
+        assertEquals(identifier, results[0]);
+        
+        results = search.findEquals(field6);
+        assertEquals(1, results.length);
+        assertEquals(identifier, results[0]);
+        
+        results = search.findEquals(field7);
+        assertEquals(1, results.length);
+        assertEquals(identifier, results[0]);
+        
+    }
+    
+    @Test
+    public void testRemovingIdentifierBasicCaseAVL()
+    {
+		File testingFile = new File("testingFile.dat");
+		if(testingFile.exists())
+			testingFile.delete();
+		
+		File testing = new File("testing.dat");
+		if(testing.exists())
+			testing.delete();
+		
+		
+        ChunkedAccess file1 = new RAFile(new File("testingFile.dat"),16, BinaryFileNode.MAX_SIZE);
+        FieldDatabase fieldDB = new AVLFieldDatabase(file1);
+        ChunkedAccess file2 = new RAFile(new File("testing.dat"), 16, BinaryFileNode.MAX_SIZE);
+        IdentifierDatabase identDB = new IdentifierDatabase(file2);
+        
+        IntegratedFileDatabase database = new IntegratedFileDatabase(fieldDB, identDB);
+        String identifier = "filename1";
+        Indexer indexer = new Indexer(database, identifier);
+        FieldSearch search = new FieldSearch(database);
+        
+        Field field = new Field("name","value");
+        
+        indexer.addField(field);
+
+        String[] results = search.findEquals(field);
+        assertEquals(1, results.length);
+        assertEquals(identifier, results[0]);
+
+        database.delete(field, identifier);
+
+        results = search.findEquals(field);
+        assertEquals(0,results.length);
+    }
+    
+    @Test
+    public void testRemovingIdentifierBasicCaseBinary()
+    {
+		File testingFile = new File("testingFile.dat");
+		if(testingFile.exists())
+			testingFile.delete();
+		
+		File testing = new File("testing.dat");
+		if(testing.exists())
+			testing.delete();
+		
+		
+        ChunkedAccess file1 = new RAFile(new File("testingFile.dat"),16, BinaryFileNode.MAX_SIZE);
+        FieldDatabase fieldDB = new BinaryTreeFieldDatabase(file1);
+        ChunkedAccess file2 = new RAFile(new File("testing.dat"), 16, BinaryFileNode.MAX_SIZE);
+        IdentifierDatabase identDB = new IdentifierDatabase(file2);
+        
+        IntegratedFileDatabase database = new IntegratedFileDatabase(fieldDB, identDB);
+        String identifier = "filename1";
+        Indexer indexer = new Indexer(database, identifier);
+        FieldSearch search = new FieldSearch(database);
+        
+        Field field = new Field("name","value");
+        
+        indexer.addField(field);
+
+        String[] results = search.findEquals(field);
+        assertEquals(1, results.length);
+        assertEquals(identifier, results[0]);
+
+        database.delete(field, identifier);
+
+        results = search.findEquals(field);
+        assertEquals(0,results.length);
+    }
+
+
+    
+    
+    
 }
